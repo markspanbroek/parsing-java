65699d5dfad1b1b8e1c56902e25db6e9d654f232
Mark Spanbroek <mark.spanbroek@philips.com>
2013-12-28 21:20:51 +0100

    add calculator example

---
+++ src/net/spanbroek/parsing/example/Calculator.java
@@ -0,0 +1,74 @@
+package net.spanbroek.parsing.example;
+
+import net.spanbroek.parsing.Parser;
+import net.spanbroek.parsing.Rule;
+import net.spanbroek.parsing.Transformation;
+
+import java.util.List;
+
+import static net.spanbroek.parsing.Parsing.*;
+
+public class Calculator {
+
+    Rule expression = rule();
+    Rule expression1 = rule();
+    Rule addition = rule();
+    Rule subtraction = rule();
+    Rule braces = rule();
+    Rule integer = rule();
+
+    public Calculator() {
+        expression.is(
+                choice(expression1, addition, subtraction)
+        );
+        expression1.is(
+                choice(braces, integer)
+        );
+        addition.is(
+                concat(expression, literal("+"), expression1)
+        );
+        subtraction.is(
+                concat(expression, literal("-"), expression1)
+        );
+        braces.is(
+                concat(literal("("), expression, literal(")"))
+        );
+        integer.is(
+                range('0','9')
+        );
+
+        integer.transform(new Transformation() {
+            @Override
+            public Object transform(List<Object> result) {
+                return Integer.parseInt((String) result.get(0));
+            }
+        });
+        addition.transform(new Transformation() {
+            @Override
+            public Object transform(List<Object> result) {
+                int left = (Integer)result.get(0);
+                int right = (Integer)result.get(2);
+                return left + right;
+            }
+        });
+        subtraction.transform(new Transformation() {
+            @Override
+            public Object transform(List<Object> result) {
+                int left = (Integer)result.get(0);
+                int right = (Integer)result.get(2);
+                return left - right;
+            }
+        });
+        braces.transform(new Transformation() {
+            @Override
+            public Object transform(List<Object> result) {
+                return result.get(1);
+            }
+        });
+    }
+
+    public int evaluate(String expression) {
+        Parser parser = this.expression;
+        return (Integer) parser.parse(expression);
+    }
+}
---
+++ test/net/spanbroek/parsing/example/CalculatorTests.java
@@ -0,0 +1,38 @@
+package net.spanbroek.parsing.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTests {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void shouldEvaluateSingleDigitInteger() {
+        for (int i=0; i<10; i++) {
+            assertEquals(i, calculator.evaluate("" + i));
+        }
+    }
+
+    @Test
+    public void shouldEvaluateAddition() {
+        assertEquals(2, calculator.evaluate("1+1"));
+    }
+
+    @Test
+    public void shouldEvaluateSubtraction() {
+        assertEquals(1, calculator.evaluate("2-1"));
+    }
+
+    @Test
+    public void shouldAllowCombinationOfSubtractionAndAddition() {
+        assertEquals(2, calculator.evaluate("2-1+1"));
+    }
+
+    @Test
+    public void shouldAllowBraces() {
+        assertEquals(0, calculator.evaluate("2-(1+1)"));
+    }
+
+}
